974d6722790fe992e4347697d125b8627ad7c99f
MikeXado <mike.gurin21@gmail.com>
2024-11-27 18:39:21 +0200

    fix build

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import SignIn from '../../libs/components/views/Login/Login'
 
 export default function Page() {
-  return <SignIn />
+  return (
+    <Suspense>
+      <SignIn />
+    </Suspense>
+  )
 }
